e816fb7e0befe82432e6a604193618fb2080d924
Fernanda <fernanda.andrademoya@coolblue.nl>
2021-02-03 18:05:35 +0100

    Choose a message randomly

---
+++ message_bot.py
@@ -0,0 +1,5 @@
+import random
+from messages import MESSAGES_GOOD_MORNING
+from messages import MESSAGES_GOOD_BYE
+
+print(random.choice(MESSAGES_GOOD_MORNING))
---
+++ messages.py
@@ -0,0 +1,19 @@
+MESSAGES_GOOD_MORNING = [
+    "Good morning!", 
+    "Bom dia!", 
+    "Goedemorgen",
+    "Buenos dias",
+    "Morning!",
+    "Happy morning team!",
+    "Goood morning!",
+    ":wave::skin-tone-3: buenos dias:sunny::coffee:"
+]
+
+MESSAGES_GOOD_BYE = [
+    "Have a nice evening!",
+    "See you tomorrow! :wave:",
+    "See you guys tomorrow!",
+    "Tot morgen",
+    "Doei",
+    "Bye all!"
+]
